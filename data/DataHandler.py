8bf2e19988f10201b0c419aa1ac55e9b8506b8c6
Kyle He <panyanhe99@gmail.com>
2023-03-06 18:43:04 -0800

    Add quadtree

--- data/DataHandler.py
+++ data/DataHandler.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from pathlib import Path
 
 from datetime import datetime
@@ -7,7 +8,7 @@ from astropy import units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 
-from helpers.QuadTree import Index
+from data.helpers.QuadTree import Index
 
 class StarData:
     def __init__(self):
@@ -21,12 +22,25 @@ class StarData:
         path = Path(__file__).parent / "star-data.csv"
         with open(path, newline='') as reader:
             self.db = [{k: v for k, v in row.items()} for row in csv.DictReader(reader, skipinitialspace=True)]
+
+        self.star_locations = Index(bbox = (0, -90, 24, 90))
+
+        path = Path(__file__).parent / "star-data.json"
+        with open(path) as reader:
+            star_location_list = json.load(reader)
+            for star_location in star_location_list:
+                ra = float(star_location["ra"])
+                dec = float(star_location["dec"])
+                self.star_locations.insert(star_location["id"], bbox= (ra, dec, ra, dec))
     
     def find_star(self, attribute: "id", value):
         for star_data in self.db:
             if star_data [attribute] == value:
                 star = Star(star_data)
                 return star
+    
+    def find_closest_stars(self, ra, dec, ra_range = 0.1, dec_range = 0.375):
+        return self.star_locations.intersect(bbox = (ra - ra_range, dec - dec_range, ra + ra_range, dec + dec_range))
 
 # will store the star data: attribute
 # will also include the converters that conver to earth location
@@ -35,7 +49,7 @@ class Star:
         self.db = None
         self.data = data
 
-        self.sky_coordinates = SkyCoord(ra = float(self.data["ra"]) * u.hours, dec = float(self.data["dec"]) * u.degree) 
+        self.sky_coordinates = SkyCoord(ra = float(self.data["ra"]) * u.hourangle, dec = float(self.data["dec"]) * u.degree) 
     
     def update_location(self):
         # location of USC
